cbb81f6b7608415dda7de6efcb349733e61df5f2
Rui Barbosa <rbb_neto@hotmail.com>
2021-01-04 16:13:14 -0300

    branch prototype

--- src/pages/Classroom/index.js
+++ src/pages/Classroom/index.js
@@ -3,21 +3,14 @@ import { Link } from 'react-router-dom';
 
 import { Jutsu } from 'react-jutsu'
 
-import { store } from '../../store';
-
 const Classroom = () => {
-  const student = store.getState().user.profile.name;
-
-  console.log(student);
-
-  return (
+    return (
       <div>
           <h1>Aula</h1>
           <Link to="/">Voltar</Link>
 
           <Jutsu containerStyles={{ width: '1200px', height: '800px' }}
             roomName="sala_de_aula002"
-            displayName={student}
             onMeetingEnd={() => console.log('Meeting has ended')}
             loadingComponent={<p>loading ...</p>}
             errorComponent={<p>Oops, something went wrong</p>} 
