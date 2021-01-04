222c72a5f299804690f4b349c250a04cb1e8f0ed
Rui Barbosa <rbb_neto@hotmail.com>
2021-01-04 17:10:51 -0300

    ajuste de roteamento2

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Login from '../pages/Login';
 import SignUp from '../pages/SignUp';
 
 import Dashboard from '../pages/Dashboard';
