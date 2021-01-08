06a0721d6e8ea79cec6e6923b844c11df957803b
Rui Barbosa <rbb_neto@hotmail.com>
2021-01-08 16:12:12 -0300

    adjusted the backend URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://github.com/rubrato/EscolaOnline-backend:3333',
+    baseURL: 'https://tccbackend.azurewebsites.net:3333',
 })
 
 export default api;
